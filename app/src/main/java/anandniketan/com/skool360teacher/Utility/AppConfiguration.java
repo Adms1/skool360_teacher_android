cc0938788d9b405269879b2626e4ed07ad7e4881
adms <admsbuild@gmail.com>
2019-05-14 15:44:29 +0530

    birthday module change login bg Antra 14/05/2019 03.44

--- app/src/main/java/anandniketan/com/skool360teacher/Utility/AppConfiguration.java
+++ app/src/main/java/anandniketan/com/skool360teacher/Utility/AppConfiguration.java
@@ -14,7 +14,7 @@ public class AppConfiguration {
         LIVE, LOCAL
     }
 
-    static Domain domain = Domain.LOCAL;//only Change this for changing environment
+    static Domain domain = Domain.LIVE;//only Change this for changing environment
 
     public static String getBaseUrl() {
         String url = "";
